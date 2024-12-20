c3a177683aee2ca64887eb20ff18ee0e495554ff
程益祥 <1574641450@qq.com>
2024-12-20 15:48:28 +0800

    Update BeanUtil.java

--- manage-system/src/main/java/com/soft/base/utils/BeanUtil.java
+++ manage-system/src/main/java/com/soft/base/utils/BeanUtil.java
@@ -4,9 +4,13 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @Author: cyx
@@ -17,6 +21,11 @@ import java.util.Map;
 @Component
 public class BeanUtil {
 
+    /**
+     * 将实体类转成map
+     * @param bean
+     * @return
+     */
     public Map<String,Object> bean2map(Object bean) {
         Map<String,Object> map = new HashMap<>();
         try {
@@ -29,6 +38,31 @@ public class BeanUtil {
         } catch (InvocationTargetException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * 将map转成实体类
+     * @param map
+     * @param classType
+     * @return
+     * @param <T>
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public <T> T map2bean(Map<String,Object> map, Class<T> classType) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        T instance = classType.getConstructor().newInstance();
+        Set<String> keys = map.keySet();
+        Field[] declaredFields = instance.getClass().getDeclaredFields();
+        for (Field c : declaredFields) {
+            String name = c.getName();
+            if (keys.contains(name) && c.getType().isInstance(map.get(name))) {
+                c.setAccessible(true);
+                c.set(instance, map.get(name));
+            }
+        }
+        return instance;
     }
 
 }
