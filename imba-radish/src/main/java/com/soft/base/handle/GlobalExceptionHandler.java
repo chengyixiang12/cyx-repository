f3b1d95a44b6be9b4466951c65dee39e7c95114a
程益祥 <1574641450@qq.com>
2025-03-12 10:13:19 +0800

    完善
    新增全局捕获异常：NoResourceFoundException；
    新增spring-doc的yml配置

--- imba-radish/src/main/java/com/soft/base/handle/GlobalExceptionHandler.java
+++ imba-radish/src/main/java/com/soft/base/handle/GlobalExceptionHandler.java
@@ -1,27 +1,38 @@
 package com.soft.base.handle;
 
 import com.soft.base.resultapi.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 /**
  * 全局异常处理器
  */
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<R<Object>> methodNotSupportException(HttpRequestMethodNotSupportedException ex) {
+        log.error(ex.getLocalizedMessage());
         return new ResponseEntity<>(R.fail("不支持的请求方式"), HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<R<Object>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        return new ResponseEntity<>(R.fail("日期格式异常"), HttpStatus.BAD_REQUEST);
+        log.error(ex.getLocalizedMessage());
+        return new ResponseEntity<>(R.fail("非法日期格式"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<R<Object>> noResourceFoundException(NoResourceFoundException ex) {
+        log.error(ex.getLocalizedMessage());
+        return new ResponseEntity<>(R.fail("未找到资源"), HttpStatus.BAD_REQUEST);
     }
 }
