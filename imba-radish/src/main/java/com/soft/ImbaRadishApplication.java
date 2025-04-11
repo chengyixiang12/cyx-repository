69dd141c517de009a8eecfd6ca933812ee3e2158
程益祥 <1574641450@qq.com>
2025-04-11 10:57:02 +0800

    java
    优化环境变量加载

--- imba-radish/src/main/java/com/soft/ImbaRadishApplication.java
+++ imba-radish/src/main/java/com/soft/ImbaRadishApplication.java
@@ -1,10 +1,14 @@
 package com.soft;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.CompletableFuture;
 
 @SpringBootApplication
 @MapperScan({"com.soft.cyx.mapper","com.soft.base.mapper"})
@@ -14,5 +18,14 @@ public class ImbaRadishApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImbaRadishApplication.class, args);
+    }
+
+    /**
+     * 加载环境变量
+     */
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().parallelStream().forEach((e -> System.setProperty(e.getKey(), e.getValue())));
     }
 }
