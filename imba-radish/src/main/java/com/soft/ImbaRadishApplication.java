aa70d337677daabe118f2eab7491cb7579ad4104
程益祥 <1574641450@qq.com>
2025-05-15 11:31:10 +0800

    优化
    隐私配置使用环境配置参数代替

--- imba-radish/src/main/java/com/soft/ImbaRadishApplication.java
+++ imba-radish/src/main/java/com/soft/ImbaRadishApplication.java
@@ -1,10 +1,10 @@
 package com.soft;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
+import com.soft.base.handle.EnvLoaderHandler;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,15 +15,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ImbaRadishApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ImbaRadishApplication.class, args);
-    }
-
-    /**
-     * 加载环境变量
-     */
-    @PostConstruct
-    public void loadEnv() {
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().parallelStream().forEach((e -> System.setProperty(e.getKey(), e.getValue())));
+        new SpringApplicationBuilder(ImbaRadishApplication.class).initializers(new EnvLoaderHandler()).run(args);
     }
 }
---
+++ imba-radish/src/main/java/com/soft/base/handle/EnvLoaderHandler.java
@@ -0,0 +1,36 @@
+package com.soft.base.handle;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @Author: cyx
+ * @Description: TODO
+ * @DateTime: 2025/5/15 10:55
+ **/
+public class EnvLoaderHandler implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext context) {
+        try (InputStream is = new FileInputStream(".env")) {
+            Properties props = new Properties();
+            props.load(is);
+
+            Map<String, Object> map = new HashMap<>();
+            props.forEach((key, value) -> map.put(String.valueOf(key), value));
+
+            // 加入 Spring 的 Environment 中
+            MapPropertySource propertySource = new MapPropertySource("custom-env", map);
+            context.getEnvironment().getPropertySources().addFirst(propertySource);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load .env file", e);
+        }
+    }
+}
