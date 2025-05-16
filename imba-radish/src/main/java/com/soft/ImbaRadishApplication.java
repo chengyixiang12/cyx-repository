1baa2400ac25b0bf23b47a80737a6a409abf2c66
程益祥 <1574641450@qq.com>
2025-05-16 17:53:14 +0800

    Update ImbaRadishApplication.java

--- imba-radish/src/main/java/com/soft/ImbaRadishApplication.java
+++ imba-radish/src/main/java/com/soft/ImbaRadishApplication.java
@@ -2,7 +2,6 @@ package com.soft;
 
 import com.soft.base.handle.EnvLoaderHandler;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
