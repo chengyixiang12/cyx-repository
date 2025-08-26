c399d774d53cdc270b5c852c3976fc808941fbdd
程益祥 <1574641450@qq.com>
2025-08-26 15:28:12 +0800

    修复

--- imba-radish-ui/vite.config.ts
+++ imba-radish-ui/vite.config.ts
@@ -21,5 +21,20 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src')
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // 所有入口点使用相同命名模式
+        entryFileNames: 'js/[hash].js',
+        // 对chunk使用数字序列而不是基于组件名
+        chunkFileNames: 'js/[hash].js',
+        // 手动配置chunk分割
+        manualChunks: {
+          vendor: ['vue', 'vue-router'],
+          // 其他依赖分组...
+        }
+      }
+    }
+  }
 })
 
